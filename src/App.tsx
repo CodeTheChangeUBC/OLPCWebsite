acf77c279cb5c576b1a4429b30562007c4c039f1
lll <or.samuel1@gmail.com>
2018-10-25 18:33:37 -0700

    added files for banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">OLPC</h1>
         </header>
+        <nav className="navbar">
+          <ul>
+            <li>
+              <a>
+                1 Button
+              </a>
+            </li>
+            <li>
+              <a>
+                2 Button
+              </a>
+            </li>
+            <li>
+              <a>
+                3 Button
+              </a>
+            </li>
+          </ul>
+        </nav>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ src/Banner/Banner.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Banner extends React.Component {
+    public render() {
+        return (
+            <header>
+
+            </header>
+        )
+    }
+} 
+
+export default Banner;
