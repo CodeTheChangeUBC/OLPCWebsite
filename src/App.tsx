d0acf73ce6782ddfc910d0be8a3e7e35f6f33af3
Gaurav Vasudev <gaurav.nv1@gmail.com>
2018-11-01 17:54:16 -0700

    Footer rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import * as React from 'react';
 import './App.css';
+import Footer from './Components/Footer/Footer';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Welcome to OLPC</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.tsx</code> and save to reload.
+          </p>
+        </div>
+        <div>
+          <Footer />
+        </div>
       </div>
     );
   }
---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './Footer.css';
+
+export const Footer =() => {
+ 
+    return (
+      <div className="Footer">
+        <header className="Footer-header">
+          <h1 className="Footer-text">OLPC in collaboration with Code the Change, UBC</h1>
+        </header>
+        {/* <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p> */}
+      </div>
+    );
+  
+}
+
+export default Footer;
