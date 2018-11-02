309c202ff7f6bc5d5ad9488fecd31ea42afe1273
lll <or.samuel1@gmail.com>
2018-11-01 17:58:16 -0700

    added banner

--- src/Banner/Banner.tsx
+++ src/Banner/Banner.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
+import './Banner.css';
 
-class Banner extends React.Component {
-    public render() {
+export const Banner = () => {
+    
         return (
-            <header>
-
-            </header>
+        <header>
+          O
+          <span className = "hide">ne</span>&nbsp;
+          L
+          <span className = "hide">aptop</span>&nbsp;
+          P
+          <span className = "hide">er</span>&nbsp;
+          C
+          <span className = "hide">hild</span>
+        </header>
         )
-    }
+    
 } 
 
 export default Banner;
