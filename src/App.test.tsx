1bcbcef469f7c44bf6cdeb001f545413989266e5
lll <or.samuel1@gmail.com>
2018-11-08 15:03:53 -0800

    added image component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+import Banner from './Banner/Banner';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Banner />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,16 @@
 import * as React from 'react';
 import './App.css';
+import Banner from './Banner/Banner';
+import Image from './Image/Image';
 
 // import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">OLPC</h1>
-        </header>
-        <nav className="navbar">
-          <ul>
-            <li>
-              <a>
-                1 Button
-              </a>
-            </li>
-            <li>
-              <a>
-                2 Button
-              </a>
-            </li>
-            <li>
-              <a>
-                3 Button
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div> 
+        <Banner />
+        <Image />
       </div>
     );
   }
