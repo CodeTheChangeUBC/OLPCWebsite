58e3b039d43af00f256ee7c162a0988bc27e5a73
Gaurav Vasudev <gaurav.nv1@gmail.com>
2018-11-15 17:08:40 -0800

    Footer Done

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,8 @@ class App extends React.Component {
   public render() {
     return (
       <div>
-        <div className="App">
-          <header className="App-header">
-            <h1 className="App-title">Welcome to OLPC</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.tsx</code> and save to reload.
-          </p>
-        </div>
-        <div>
-          <Footer />
-        </div>
+       
+       <Footer />
       </div>
     );
   }
--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -5,12 +5,13 @@ export const Footer =() => {
  
     return (
       <div className="Footer">
-        <header className="Footer-header">
-          <h1 className="Footer-text">OLPC in collaboration with Code the Change, UBC</h1>
-        </header>
-        {/* <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p> */}
+        <footer className="bg-near-black white-80 pv5 pv6-l ph4">
+          <p className="f6"><span className="dib mr4 mr5-ns">Made in collaboration with Code the Change UBC</span>
+            <a className="link white-80 hover-light-purple" href="/terms">Terms</a> /
+            <a className="link white-80 hover-gold" href="/privacy"> Privacy </a> /
+            <a className="link white-80 hover-green" href="#">gaurav@codethechange.ca </a>
+          </p>
+        </footer>
       </div>
     );
   
